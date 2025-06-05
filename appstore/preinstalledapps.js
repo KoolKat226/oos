9a5c159194ba0af2cccf264ee6e6c2db90ed1e73
KoolKat <99701844+KoolKat226@users.noreply.github.com>
2025-06-04 20:07:28 -0400

    Create preinstalledapps.js

---
+++ appstore/preinstalledapps.js
@@ -0,0 +1,60 @@
+// preinstalledapps.js
+var preinstalledapps = [
+  {
+    name: "OOSmoji",
+    developer: "OlexOS",
+    icon: "./oosmoji_icon.png",
+    preview: "https://cdn2.unrealengine.com/Fortnite%2Fblog%2Ficon-series%2FEN_11BR_IconSeries_Social_Ninja_Solo-1920x1080-9dce8e08dee863c2d36d841df7062045a966285f.jpg",
+    code: "*oosango*iframe name*ooseq**oosap*minecraft*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*./miimaker.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "Minecraft",
+    developer: "Jackson Coolman",
+    icon: "https://cdn2.steamgriddb.com/icon_thumb/2800a901597876b19a05fa2ba197a966.png",
+    preview: "https://media.gamestop.com/i/gamestop/10108713_10113355_10114672_10115479_10115481_10117341_10130409_10151161_10155679_10162373_SCR09?$screen$",
+    code: "*oosango*iframe frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io*oosfsl*oosg*oosfsl*index.html?game*ooseq*minecraft*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "My Games",
+    developer: "Jackson Coolman",
+    icon: "https://cdn.glitch.global/6cf99900-c3ba-4c81-8866-8f88772e629b/mygames2.0.png",
+    preview: "https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    code: "*oosango*iframe name*ooseq**oosap*mygames*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io*oosfsl*ovalgames*oosfsl*normal.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "Stickman Hook",
+    developer: "Jackson Coolman",
+    icon: "https://play-lh.googleusercontent.com/yXqBI1Vb1b-xjwmXLE2T7FQbgHIxfvjze1b6jO2aLHfy8Y8ta7NFSEVqZoPHtM2Ajyrl",
+    preview: "https://i.ytimg.com/vi/xe6XXA9KfJw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCytopjFGZbb0paq6ytutpdRQ8T8w",
+    code: "*oosango*iframe name*ooseq**oosap*minecraft*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*ug17.github.io/play/Stickman-HookUB/*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "Super Mario 64",
+    developer: "Nintendo",
+    icon: "https://nintendocore.org/cdn/shop/products/Super-Mario-64-1.png?v=1568966086",
+    preview: "https://tvobsessive.com/wp-content/uploads/2020/02/sm1.jpg",
+    code: "*oosango*iframe name*ooseq**oosap*superm64*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https://olexminceraft.glitch.me/indexmario.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "Sandboxels",
+    developer: "Jackson Coolman",
+    icon: "https://cdn2.steamgriddb.com/logo_thumb/13b5241a19c9807853e0bda1c0d29d8b.png",
+    preview: "https://www.metacritic.com/a/img/resize/667abc304d246cedc72285bdba83d1dae7ed6825/catalog/provider/6/12/6-1-1115160-52.jpg?auto=webp&fit=crop&height=675&width=1200",
+    code: "*oosango*iframe frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*sandboxels.r74n.com*oosfsl**oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "Retro Bowl",
+    developer: "Jackson Coolman",
+    icon: "https://play-lh.googleusercontent.com/WRM5Y1xZmzcCP1YtO5zl6G2g7CU5c5ZfjX4UVrgi1bpNgkfy-wuB-bQx3kkeRfaGYQ=w240-h480-rw",
+    preview: "https://i.guim.co.uk/img/media/0921e08475593355de6e25d497e280009de8833c/43_0_1398_839/master/1398.jpg?width=465&dpr=1&s=none&crop=none",
+    code: "*oosango*iframe name*ooseq**oosap*retrobowl*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*ug17.github.io*oosfsl*play*oosfsl*Retro-Bowl*oosfsl**oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  {
+    name: "Spelunky",
+    developer: "Jackson Coolman",
+    icon: "https://koolkat226.github.io/oosg/spelunky/src/icon.png",
+    preview: "https://koolkat226.github.io/oosg/spelunky/src/icon.png",
+    code: "*oosango*iframe name*ooseq**oosap*retrobowl*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io/oosg/index.html?game=spelunky/src*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+  },
+  // …at least 8 entries total…
+];
