9813afa23022a84d05ff1abf3029ea5b1abf3102
KoolKat <99701844+KoolKat226@users.noreply.github.com>
2025-06-11 15:19:02 -0400

    Update preinstalledapps.js

--- appstore/preinstalledapps.js
+++ appstore/preinstalledapps.js
@@ -3,23 +3,23 @@ var preinstalledapps = [
   {
     name: "OOSmoji",
     developer: "OlexOS",
-    icon: "./oosmoji_icon.png",
+    icon: "./images/oosmojiicon.png",
     preview: "https://cdn2.unrealengine.com/Fortnite%2Fblog%2Ficon-series%2FEN_11BR_IconSeries_Social_Ninja_Solo-1920x1080-9dce8e08dee863c2d36d841df7062045a966285f.jpg",
     code: "*oosango*iframe name*ooseq**oosap*minecraft*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*./miimaker.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
   },
   {
     name: "Minecraft",
     developer: "Jackson Coolman",
-    icon: "https://cdn2.steamgriddb.com/icon_thumb/2800a901597876b19a05fa2ba197a966.png",
+    icon: "https://koolkat226.github.io/oosg/minecraft/icon.png",
     preview: "https://media.gamestop.com/i/gamestop/10108713_10113355_10114672_10115479_10115481_10117341_10130409_10151161_10155679_10162373_SCR09?$screen$",
     code: "*oosango*iframe frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io*oosfsl*oosg*oosfsl*index.html?game*ooseq*minecraft*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
   },
   {
-    name: "My Games",
+    name: "Browser",
     developer: "Jackson Coolman",
-    icon: "https://cdn.glitch.global/6cf99900-c3ba-4c81-8866-8f88772e629b/mygames2.0.png",
-    preview: "https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    code: "*oosango*iframe name*ooseq**oosap*mygames*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io*oosfsl*ovalgames*oosfsl*normal.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+    icon: "./images/browsericon.png",
+    preview: "",
+    code: "*oosango*iframe frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*.*oosfsl*browser*oosfsl*index.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
   },
   {
     name: "Stickman Hook",
@@ -31,16 +31,16 @@ var preinstalledapps = [
   {
     name: "Super Mario 64",
     developer: "Nintendo",
-    icon: "https://nintendocore.org/cdn/shop/products/Super-Mario-64-1.png?v=1568966086",
+    icon: "https://koolkat226.github.io/oosg/mario64/icon.png",
     preview: "https://tvobsessive.com/wp-content/uploads/2020/02/sm1.jpg",
-    code: "*oosango*iframe name*ooseq**oosap*superm64*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https://olexminceraft.glitch.me/indexmario.html*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+    code: "*oosango*iframe name*ooseq**oosap*superm64*oosap* frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io*oosfsl*oosg*oosfsl*index.html?game*ooseq*mario64*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
   },
   {
-    name: "Sandboxels",
+    name: "Doodle Jump",
     developer: "Jackson Coolman",
-    icon: "https://cdn2.steamgriddb.com/logo_thumb/13b5241a19c9807853e0bda1c0d29d8b.png",
-    preview: "https://www.metacritic.com/a/img/resize/667abc304d246cedc72285bdba83d1dae7ed6825/catalog/provider/6/12/6-1-1115160-52.jpg?auto=webp&fit=crop&height=675&width=1200",
-    code: "*oosango*iframe frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*sandboxels.r74n.com*oosfsl**oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
+    icon: "https://koolkat226.github.io/oosg/doodlejump/icon.png",
+    preview: "https://shots.codepen.io/username/pen/XWMzvaM-800.jpg",
+    code: "*oosango*iframe frameborder*ooseq**oosap*0*oosap* style*ooseq**oosap*position*ooscol* absolute*oosscol* top*ooscol* 0*oosscol* left*ooscol* 0*oosscol* border*ooscol* none*oosscol* height*ooscol* 100%*oosscol* width*ooscol* 100%*oosscol**oosap* src*ooseq**oosap*https*ooscol**oosfsl**oosfsl*koolkat226.github.io*oosfsl*oosg*oosfsl*index.html?game*ooseq*doodlejump*oosap**oosangc**oosango**oosfsl*iframe*oosangc*"
   },
   {
     name: "Retro Bowl",
