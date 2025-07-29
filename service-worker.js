ec5a014f7c379fc2cd1a778358886797d036918f
KoolKat <99701844+KoolKat226@users.noreply.github.com>
2025-07-29 19:47:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // File: /service-worker.js
 // ────────────────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'oosv2.3';
+const CACHE_NAME = 'oosv2.4';
 
 // ─── Install Event: Activate Immediately ────────────────────────────
 self.addEventListener('install', (evt) => {
