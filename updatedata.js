fc6d997c9ef4911374213328f21126f20967b0a0
KoolKat <99701844+KoolKat226@users.noreply.github.com>
2025-07-29 18:56:20 -0400

    Update updatedata.js

--- updatedata.js
+++ updatedata.js
@@ -6,7 +6,7 @@
   const stored = parseFloat(localStorage.getItem('oos_version') || '0');
 
   // Bump this number whenever you release a new offline build!
-  const NEW_VERSION = 2.7;
+  const NEW_VERSION = 2;
 
   // If we already have this version (or newer), nothing to do
   if (stored >= NEW_VERSION) return;
